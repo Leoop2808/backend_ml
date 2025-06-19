e04705e0ea976ed67c302e9ec43e54b1521e7ddf
devl <devl>
2025-06-19 18:18:05 -0500

    feat: habilitar CORS para todos los dominios

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI(
     title="API de Predicción de Hipertensión",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # Permite todos los dominios
+    allow_credentials=False,    # Evita error con "*"
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router)
